f5cb6d1cf1e92ec21002b61de0e1fdb3317ecfcd
AbhimanyuJ <abhimanyu.joshi129@gmail.com>
2024-03-31 14:42:50 +0530

    98/365

--- workspace_24/react/capstone/crwn-clothing/src/routes/sign-in/sign-in.component.jsx
+++ workspace_24/react/capstone/crwn-clothing/src/routes/sign-in/sign-in.component.jsx
@@ -1,10 +1,14 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils.js'
+import { 
+    signInWithGooglePopup, 
+    createUserDocumentFromAuth, 
+    } from '../../utils/firebase/firebase.utils.js'
 
-const SignIn = () => {
+const SignIn = () => { 
+
     const logGoogleUser = async () => {
         const { user } = await signInWithGooglePopup();
         const userDocRef = await createUserDocumentFromAuth(user);
-    }
+    };
 
     return(<div>
         <h1>Sign In Page</h1>
