7f62b6e6fbf4dfb5d6f5480a322d1e908b72eaf9
AbhimanyuJ <abhimanyu.joshi129@gmail.com>
2024-04-01 15:03:29 +0530

    115/365

--- workspace_24/react/capstone/crwn-clothing/src/index.js
+++ workspace_24/react/capstone/crwn-clothing/src/index.js
@@ -1,17 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import reportWebVitals from './reportWebVitals';
+
 import App from './App';
 import { UserProvider } from './contexts/user.context';
+import { ProductProvider } from './contexts/products.context';
+
 import './index.scss';
-import reportWebVitals from './reportWebVitals';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
+        <ProductProvider>
+          <App />
+        </ProductProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
